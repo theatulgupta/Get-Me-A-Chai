7e897bc376dcfc77d98d860c8be92e3ec7081187
theatulgupta <atulgupta3058@gmail.com>
2024-05-25 09:47:54 +0530

    Initial Commit

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Footer = () => {
+  return <footer>Copyright &copy; 2024. Get Me A Chai - All rights reserved </footer>;
+};
+
+export default Footer;
---
+++ components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="text-white bg-black">
+      <ul>
+        <li></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
